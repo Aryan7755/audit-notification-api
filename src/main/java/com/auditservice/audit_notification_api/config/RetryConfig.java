534b86a53b798827bac31ca7e05eab06b8b1827a
Aryan raj <aryanbanthu@gmail.com>
2026-09-12 10:49:50 +0530

    feat: enable Spring Retry and annotate EmailService with exponential backoff

---
+++ src/main/java/com/auditservice/audit_notification_api/config/RetryConfig.java
@@ -0,0 +1,9 @@
+package com.auditservice.audit_notification_api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+
+@Configuration
+@EnableRetry
+public class RetryConfig {
+}
--- src/main/java/com/auditservice/audit_notification_api/service/EmailService.java
+++ src/main/java/com/auditservice/audit_notification_api/service/EmailService.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
@@ -19,7 +21,11 @@ public class EmailService {
 
     private final JavaMailSender mailSender;
     private final TemplateEngine templateEngine;
-
+    @Retryable(
+            retryFor = { MessagingException.class, Exception.class },
+            maxAttempts = 3,
+            backoff = @Backoff(delay = 2000, multiplier = 2)
+    )
     public void sendBudgetAlertEmail(String recipient, String category, BigDecimal limit, BigDecimal spent) throws MessagingException {
         log.info("Preparing budget alert email for recipient: {}", recipient);
 
