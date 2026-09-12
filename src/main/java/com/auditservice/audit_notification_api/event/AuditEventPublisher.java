f4e5ac3319fb480a389a38b312c9105d3500d377
Aryan raj <aryanbanthu@gmail.com>
2026-09-12 10:15:38 +0530

    feat: implement AuditEventPublisher to dispatch domain events

--- src/main/java/com/auditservice/audit_notification_api/event/AuditEventPublisher.java
+++ src/main/java/com/auditservice/audit_notification_api/event/AuditEventPublisher.java
@@ -1,4 +1,24 @@
 package com.auditservice.audit_notification_api.event;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+@RequiredArgsConstructor
 public class AuditEventPublisher {
+
+    private final ApplicationEventPublisher applicationEventPublisher;
+
+    public void publishExpenseCreated(String title, BigDecimal amount, String category, String userEmail) {
+        ExpenseCreatedEvent event = new ExpenseCreatedEvent(title, amount, category, userEmail);
+        applicationEventPublisher.publishEvent(event);
+    }
+
+    public void publishBudgetExceeded(String userEmail, String category, BigDecimal monthlyLimit, BigDecimal currentSpent) {
+        BudgetExceededEvent event = new BudgetExceededEvent(userEmail, category, monthlyLimit, currentSpent);
+        applicationEventPublisher.publishEvent(event);
+    }
 }
