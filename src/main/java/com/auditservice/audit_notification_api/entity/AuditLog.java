1390d6f957ee0032dc50afc4740e9f458f415ad9
Aryan raj <aryanbanthu@gmail.com>
2026-09-12 09:56:32 +0530

    feat: create AuditLog entity and repository

---
+++ src/main/java/com/auditservice/audit_notification_api/entity/AuditLog.java
@@ -0,0 +1,37 @@
+package com.auditservice.audit_notification_api.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "audit_logs")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AuditLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String action;
+
+    @Column(nullable = false)
+    private String userEmail;
+
+    @Column(columnDefinition = "TEXT")
+    private String details;
+
+    @Column(nullable = false)
+    private LocalDateTime timestamp;
+
+    @PrePersist
+    protected void onCreate() {
+        this.timestamp = LocalDateTime.now();
+    }
+}
