b3c4c2a785e3523c4d2258a5234f8221e703e8c1
Aryan raj <aryanbanthu@gmail.com>
2026-09-12 09:54:28 +0530

    build: initialize Spring Boot 3 project with Web, JPA, Mail, Thymeleaf, and H2

---
+++ src/main/java/com/auditservice/audit_notification_api/AuditNotificationApiApplication.java
@@ -0,0 +1,13 @@
+package com.auditservice.audit_notification_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditNotificationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditNotificationApiApplication.class, args);
+	}
+
+}
